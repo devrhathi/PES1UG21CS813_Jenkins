ae3239bdbff89b1ac735c0daf18518f420ba2edc
devrh <hathi.dev@namasyslimited.onmicrosoft.com>
2023-02-15 11:45:09 +0530

    new cpp file

---
+++ main/newfile.cpp
@@ -0,0 +1,10 @@
+// File: newfile.cpp
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "New Cpp file by PES1UG21CS813\n";
+    cout << "It is for the demonstration of pipelines\n";
+    return 0;
+}
